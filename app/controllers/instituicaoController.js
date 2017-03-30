40613dee2320ba43e66a8a2036d3b328c2eaa2e0
Rafael Borges <razborges@me.com>
2017-03-30 12:19:03 -0300

    Verificação dos dados do formulário de Instituicao

--- app/controllers/instituicaoController.js
+++ app/controllers/instituicaoController.js
@@ -12,6 +12,15 @@ module.exports.cadastrar = function(application, req, res){
     var dadosForm = req.body;
 
     req.assert('nome', 'Preenchimento obrigatório do nome da Instituição').notEmpty();
+    req.assert('responsavel', 'Preenchimento obrigatório do nome do Responsável da Instituição').notEmpty();
+    req.assert('email', '- Necessário utilizar um e-mail válido').notEmpty().isEmail();
+    req.assert('telefone', 'Preenchimento obrigatório do telefone').notEmpty();
+    req.assert('telefone', 'Telefone deve ter DDD e, com isso entre 10 e 11 caracteres.').len(14, 15);//parenteses e traço
+    req.assert('senha', '- Preenchimento obrigatório').notEmpty();
+    req.assert('senha', '- A senha deve ter entre 6 e 20 caracteres').len(6, 20);
+    req.assert('re_senha', '- Preenchimento obrigatório').notEmpty();
+    req.assert('re_senha', '- Deve ser igual a senha e ter de 6 a 20 caracteres').len(6, 20);
+    req.assert('re_senha', '- Deve ser igual a senha').equals(dadosForm.senha);
 
     var erros = req.validationErrors();
 
